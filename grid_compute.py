9cc985f371f4e6b2de688c980bc66470554bcefd
Evgenij-Gr <Evgenij-Gr@users.noreply.github.com>
2022-08-05 15:03:06 +0300

    Initial commit

---
+++ grid_compute.py
@@ -0,0 +1,44 @@
+import datetime
+import multiprocessing as mp
+import os
+import sys
+import time
+from functools import partial
+from itertools import product
+
+import yaml
+
+import grid
+import workers as wrk
+
+if __name__ == "__main__":
+    if '-h' in sys.argv or '--help' in sys.argv:
+        print("Usage: python grid_compute.py <pathToConfig>"
+              "\n    pathToConfig: full path to configuration file (e.g., \"./cfg.yaml\")")
+        sys.exit()
+
+    configName = sys.argv[1]
+    assert os.path.isfile(configName), f"Configuration file {os.path.abspath(configName)} does not exist!"
+
+    with open(configName, 'r') as f:
+        configDict = yaml.load(f, Loader=yaml.loader.SafeLoader)
+
+    startTime = datetime.datetime.today().strftime('%Y-%m-%d_%H-%M-%S')
+    taskName = configDict['task']
+    initFunc = wrk.registry['init'][taskName]
+    initResult = initFunc(configDict, startTime)
+
+    grid = grid.getGrid(configDict)
+    worker = wrk.registry['worker'][taskName]
+
+    pool = mp.Pool(mp.cpu_count())
+    start = time.time()
+    workerResult = pool.map(partial(worker, config=configDict, timeStamp=startTime, initResult=initResult), product(*grid))
+    end = time.time()
+    pool.close()
+    print(f"Took {end-start}s ({datetime.timedelta(seconds=end-start)})")
+
+    # define the post processing
+    postProcess = wrk.registry['post'][taskName]
+    postProcess(configDict, initResult, workerResult, grid, startTime)
+
